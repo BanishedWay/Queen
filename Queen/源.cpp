c369f6262e596d150328206ce2ca1eb1a9a81f96
Michael-cybersecurity <xjq+Michael-cybersecurity@users.noreply.github.com>
2020-03-26 12:02:30 +0800

    第二次更改，改变isPlace函数

--- Queen/源.cpp
+++ Queen/源.cpp
@@ -1,69 +1,50 @@
 #include<stdio.h>
-int a[10];
-int FABS(int x, int y);
+#include<math.h>
+int a[10], sum = 0;
 
-void Queen(int n);
+void Queen(int k, int n);
 
-int isPlace(int k);
+int isPlace(int k,int i);
 
 int main()
 {
-	Queen(8);
+	Queen(1, 8);
+	printf("共%d种解法\n", sum);
 	return 0;
 }
 
-int isPlace(int k)
+int isPlace(int k, int j)
 {
-	int i, flag = 1;
-	for (i = 1; i < k; i++)
+	int flag = 1;
+	for (int i = 1; i < k; i++)
 	{
-		if (a[i] == a[k] || FABS(a[i], a[k]) == FABS(i, k))
+		if (a[i] == j || fabs(a[i] - j) == fabs(i - k))
 		{
 			flag = 0;
-		}//同列a[i]==a[k]及同对角线FABS(a[i], a[k]) == FABS(i, k)返回错
+		}//同列及同对角线,flag=0.
 	}
 	return flag;
-}
+}//判断第k行第j列是否可以放置
 
-int FABS(int x, int y)
+void Queen(int k, int n)
 {
-	int t = x - y;
-	if (t > 0)
+	int i;
+	if (k > n)
 	{
-		return t;
+		for (i = 1; i < n; i++)
+		{
+			printf("%d ", a[i]);
+		}
+		printf("%d\n", a[i]);
+		sum++;
 	}
-	return -t;
-}
-
-void Queen(int n)
-{
-	int i, k = 1;
-	a[k] = 0;
-	while (k > 0)
+	for (i = 1; i <= n; i++)
 	{
-		a[k]++;
-		while (a[k] <= n && !isPlace(k))
+		if (isPlace(k, i))
 		{
-			a[k]++;
+			a[k] = i;
+			Queen(k + 1, n);
 		}
-		if (a[k] <= n)
-		{
-			if (k == n)
-			{
-				for (i = 1; i < n; i++)
-				{
-					printf("%d ", a[i]);
-				}
-				printf("%d\n", a[i]);
-			}
-			else
-			{
-				k++;
-				a[k] = 0;
-			}
-		}
-		else
-			k--;
 	}
 	return;
 }
